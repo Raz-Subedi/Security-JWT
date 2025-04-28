60ee6acbf0fa310d2e41bda10afaa027b04041e3
Raz Subedi <subedirazkumar@gmail.com>
2025-04-28 07:38:32 +0545

    Updated

--- src/main/java/com/alibou/Security_JWT/config/JwtService.java
+++ src/main/java/com/alibou/Security_JWT/config/JwtService.java
@@ -17,7 +17,7 @@ import java.util.function.Function;
 @Service
 public class JwtService {
 
-    private static final String SECRET_KEY = "a9m3IC30GPvcOw0sbbe6Sm0dn0xQ8wtj";
+    private static final String SECRET_KEY = "66bd1b1ef085f582534a8810a2d52c5906aa43dcb610d08b956eb8f8a5e8668ceb617700a7ddaa95b54b7a909e965350183c0e85e3687bef6c35deec04868742d4f6fba47f4ec6bfdfd358f759b896275cf1512069cbca3c6640a2cb8926f1fd5cb1963aabc284bfa570c101839e2426de4bc2144c61bcdac1b7cd02c5b1ed94cd0e9ee0dea4f5fd3059df225d538b3bf80811b3ec0432974b00f47da3d5aab843682d7e4c77a80152e8f378e7aa0c42290f366c40c752a85e4979796d529daf2a533dfc770775c8e034908992516d56e3651c0069ca79873babef5c0f709373eed3de1f5a307d6c1df5f0eda24d5170382c1199fc6698c4e2005890a4e98d0c";
 
     public String extractUsername(String token) {
         return extractClaims(token,Claims::getSubject);
@@ -28,7 +28,7 @@ public class JwtService {
         return claimsResolver.apply(claims);
     }
 
-    private String generateToken(UserDetails userDetails){
+    public String generateToken(UserDetails userDetails){
         return generateToken(new HashMap<>(),userDetails);
     }
 
